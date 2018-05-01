d4317b0d4aa2b292cb0737a2db79306ad5532e7a
mmochi23 <mmochi23@gmail.com>
2018-05-01 14:48:54 +0900

    action update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :members
+#  resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'login', to: 'members#login'
+  post 'members', to 'members#create'
+  post 'member/login', to: 'members#login'
+  post 'member/show', to: 'members#show'
 end
